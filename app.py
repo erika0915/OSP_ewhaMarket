ba387fd597eef766d59f6fcf15605ee9ae5ac900
erika0915 <soohee2001@gmail.com>
2024-10-23 20:22:40 +0900

    그룹 과제2 commit

--- app.py
+++ app.py
@@ -3,42 +3,42 @@ import sys
 application = Flask(__name__)
 
 @application.route("/")
-def hello():
-    return render_template ("index.html")
-@application.route("/list") 
+def apply():
+    return render_template ("apply.html")
+
+@application.route("/products") 
 def view_list():
-    return render_template("list.html")
-@application.route("/review") 
+    return render_template("all_products.html")
+
+@application.route("/reviews") 
 def view_review():
-    return render_template("review.html")
-@application.route("/reg_items") 
+    return render_template("view_review.html")
+
+@application.route("/mypage") 
 def reg_item():
-    return render_template("reg_items.html")
-@application.route("/reg_reviews") 
+    return render_template("my_page.html")
+
+@application.route("/new_products") 
 def reg_review():
-    return render_template("reg_reviews.html")
+    return render_template("new_products.html")
 
 @application.route("/submit_item")
-def reg_item_submit():
-    name=request.args.get("name") 
-    seller=request.args.get("seller") 
-    addr=request.args.get("addr") 
-    email=request.args.get("email") 
-    category=request.args.get("category") 
-    card=request.args.get("card")
-    status=request.args.get("status") 
-    phone=request.args.get("phone")
-
-    print(name,seller,addr,email,category,card,status,phone) 
-    return render_template("reg_item.html")
+def reg_item_submit_get():
+    name=request.args.get("product-name")
+    seller=request.args.get("seller-id")
+    location=request.args.get("location")
+    price=request.args.get("price")
+    condition=request.args.get("condition")
+    description=request.args.get("description")
+    print(name,seller,location,price,condition,description)
 
 @application.route("/submit_item_post", methods=['POST']) 
 def reg_item_submit_post():
-    image_file=request.files["file"] 
-    image_file.save("static/images/{}".format(image_file.filename))
+    image_file=request.files["photo"] 
+    image_file.save("static/image/{}".format(image_file.filename))
 
     data=request.form
-    return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
+    return render_template("result.html", data=data, img_path="static/image/{}".format(image_file.filename))
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0')
